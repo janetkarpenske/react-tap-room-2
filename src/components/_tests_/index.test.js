c3929cf7babc85cedf33fc53786306f61ca55755
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-04 11:46:58 -0800

    Pass test to confirm editing reducer and root reducer match default states

--- src/components/_tests_/index.test.js
+++ src/components/_tests_/index.test.js
@@ -1,6 +1,7 @@
 import rootReducer from './../../reducers/index';
 import { createStore } from 'redux';
 import formVisibleReducer from './../../reducers/form-visible-reducer';
+import editingReducer from './../../reducers/editing-reducer';
 import kegListReducer from './../../reducers/keg-list-reducer';
 import * as c from './../actions/ActionTypes';
 
@@ -20,6 +21,9 @@ describe("rootReducer", () => {
   });
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+  test('Check that initial state of editingReducer matches root reducer', () => {
+    expect(store.getState().editing).toEqual(editingReducer(undefined, { type: null }));
   });
   test('Check that ADD_KEG action works for kegListReducer and root reducer', () => {
     const action = {
