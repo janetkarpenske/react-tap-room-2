5c64d493627b182f2d1c927cf3437cef0b09b276
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-04 09:29:11 -0800

    Fail test correctly to toggle form in toogle-form-reducer

---
+++ src/components/_tests_/form-visible-reducer.test.js
@@ -0,0 +1,10 @@
+import formVisibleReducer from './../../reducers/form-visible-reducer';
+import * as c from './../actions/ActionTypes';
+
+describe("formVisibleReducer", () => {
+
+  test('Should toggle form visibility state to true', () => {
+    expect(formVisibleReducer(false, { type: c.TOGGLE_FORM })).toEqual(true);
+  });
+
+});
