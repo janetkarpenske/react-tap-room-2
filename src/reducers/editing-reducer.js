c4f4a846e79bc866d2dc0dd66e1b1c9abb3044d9
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-04 11:40:12 -0800

    Pass test to toggle editing true and false through reducer

--- src/reducers/editing-reducer.js
+++ src/reducers/editing-reducer.js
@@ -2,7 +2,8 @@ import * as c from './../components/actions/actionTypes';
 
 export default (state = false, action) => {
   switch (action.type) {
-  
+    case c.TOGGLE_EDIT:
+      return !state;
   default:
     return state;
   }
