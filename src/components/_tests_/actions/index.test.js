59f6198fba017a675dd0cb981bf6f7cd80c4fd83
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-04 08:47:47 -0800

    Add index.js file to actions and test actions

---
+++ src/components/_tests_/actions/index.test.js
@@ -0,0 +1,29 @@
+import * as actions from './../../actions';
+import * as c from './../../actions/actionTypes';
+
+describe('help queue actions', () => {
+  it('should create delete keg action', () => {
+    expect(actions.deleteKeg(1)).toEqual({
+      type: c.DELETE_KEG,
+      id: 1
+    });
+  });
+
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: c.TOGGLE_FORM
+    });
+  });
+
+  it('addKeg should create ADD_KEG action', () => {
+    expect(actions.addKeg({name: 'Porter', brand: 'Deschutes', price: 6, alcoholContent: 8, amountLeft: 124, id: 1})).toEqual({
+      type: c.ADD_KEG,
+      name: 'Porter', 
+      brand: 'Deschutes', 
+      price: 6, 
+      alcoholContent: 8, 
+      amountLeft: 124, 
+      id: 1
+    });
+  });
+});
---
+++ src/components/actions/index.js
@@ -0,0 +1,21 @@
+export const deleteKeg = id => ({
+  type: 'DELETE_KEG',
+  id
+});
+
+export const toggleForm = () => ({
+  type: 'TOGGLE_FORM'
+});
+
+export const addKeg = (keg) => {
+  const { name, brand, price, alcoholContent, amountLeft, id } = keg;
+  return {
+    type: 'ADD_KEG',
+    name: name,
+    brand: brand,
+    price: price,
+    alcoholContent: alcoholContent,
+    amountLeft: amountLeft,
+    id: id
+  }
+}
