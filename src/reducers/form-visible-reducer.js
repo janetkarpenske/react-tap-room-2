abd1a232ed78a7419b1d11dcd9a250bb2efb22d9
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-04 09:29:50 -0800

    Pass test to toggle form

--- src/reducers/form-visible-reducer.js
+++ src/reducers/form-visible-reducer.js
@@ -3,7 +3,7 @@ import * as c from './../components/actions/ActionTypes';
 export default (state = false, action) => {
   switch (action.type) {
   case c.TOGGLE_FORM:
-    
+    return !state;
   default:
     return state;
   }
