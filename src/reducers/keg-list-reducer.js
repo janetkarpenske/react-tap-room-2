21ba716eea010fafbf11a379ef1254a88c6c0515
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-04 09:05:47 -0800

    Test to return defaul state passes

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,5 +1,10 @@
 import * as c from './../components/actions/ActionTypes';
 
 export default (state = {}, action) => {
+  const { names, location, issue, id } = action;
+  switch (action.type) {
   
+    default:
+    return state;
+  }
 }
