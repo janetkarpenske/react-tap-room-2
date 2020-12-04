90e64fac125bee9b7efec6fdb3617ce987ad6eb4
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-04 09:34:26 -0800

    Correctly fail test to return default state from root reducer

---
+++ src/components/_tests_/index.test.js
@@ -0,0 +1,21 @@
+import rootReducer from './../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from './../../reducers/form-visible-reducer';
+import kegListReducer from './../../reducers/keg-list-reducer';
+import * as c from './../actions/ActionTypes';
+
+let store = createStore(rootReducer);
+
+describe("rootReducer", () => {
+
+  test('Should return default state if no action type is specified', () => {
+    expect(rootReducer({}, { type: null })).toEqual({
+      masterKegList: {},
+      formVisibleOnPage: false
+    });
+  });
+
+
+
+
+});
