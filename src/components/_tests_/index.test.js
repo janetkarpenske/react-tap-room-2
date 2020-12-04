584cf5070142557ff62eb8931ef96eeefdc892f9
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-04 09:37:59 -0800

    Pass test to confirm that itital state of form visible reducer matches root reducer

--- src/components/_tests_/index.test.js
+++ src/components/_tests_/index.test.js
@@ -17,7 +17,9 @@ describe("rootReducer", () => {
   test('Check that initial state of kegListReducer matches root reducer', () => {
     expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, { type: null }));
   });
-
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
 
 
 });
